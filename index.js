71c65ffe109eb39133cb0aa557690309de081f0d
Ramon <ramon.pereira@prof.unibh.br>
2021-11-03 21:24:10 -0300

    Created new routes and deleted web.config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-port = process.env.PORT
+port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
 
 app.get('/teste', (req, res) => {
   res.send('Hello World! Teste')
+})
+
+app.get('/cruzeiro', (req, res) => {
+  res.send('Melhor time do Brasil')
+})
+
+app.get('/atletico', (req, res) => {
+  res.send('Não tem BI')
+})
+
+app.get('/palmeiras', (req, res) => {
+  res.send('Não tem Mundial')
 })
 
 app.listen(port, () => {
